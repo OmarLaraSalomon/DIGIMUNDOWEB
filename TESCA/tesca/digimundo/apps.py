b17c9d9ef426eb12ab8e5da77498c8792d00e99f
OmarLaraSalomon <omar.laso0001@gmail.com>
2021-12-03 12:36:13 -0600

    menu

---
+++ TESCA/tesca/digimundo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DigimundoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'digimundo'
---
+++ TESCA/tesca/digimundo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def carrusel(request):
+  return render(request, 'social/carrusel.html')
+
+# Create your views here.
